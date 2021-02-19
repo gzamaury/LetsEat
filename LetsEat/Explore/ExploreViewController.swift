5449fced6dd932bcbb5a130ffabace3997571317
AmauryGz <gzamaury@hotmail.com>
2021-02-19 16:51:55 -0600

    Adding a Cocoa Touch Class file to your project

---
+++ LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by MacGza on 19/02/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
