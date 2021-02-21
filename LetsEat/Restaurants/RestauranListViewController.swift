fcc3a0d93ea4ef0f3c087786c170db6d7eff0e90
AmauryGz <gzamaury@hotmail.com>
2021-02-20 22:41:04 -0600

    Adopting the delegate and data source protocols

--- LetsEat/Restaurants/RestauranListViewController.swift
+++ LetsEat/Restaurants/RestauranListViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class RestauranListViewController: UIViewController {
-
+class RestauranListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+    }
 
     /*
     // MARK: - Navigation
