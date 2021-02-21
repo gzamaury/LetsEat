eccbdb39fbe114b572bf56cfd90863872be9c8be
AmauryGz <gzamaury@hotmail.com>
2021-02-20 22:27:18 -0600

    Declaring the RestauranListViewController class

---
+++ LetsEat/Restaurants/RestauranListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestauranListViewController.swift
+//  LetsEat
+//
+//  Created by MacGza on 20/02/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import UIKit
+
+class RestauranListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
