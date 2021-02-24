43a16bc6e3644148a72e10fe9eea89b49e50c422
AmauryGz <gzamaury@hotmail.com>
2021-02-24 16:50:53 -0600

    Connecting the table view to LocationViewController

---
+++ LetsEat/Location/LocationViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LocationViewController.swift
+//  LetsEat
+//
+//  Created by MacGza on 24/02/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
