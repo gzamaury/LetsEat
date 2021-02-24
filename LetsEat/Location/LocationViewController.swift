63c63e389d911129724102384b89469362ee3b3c
AmauryGz <gzamaury@hotmail.com>
2021-02-24 17:23:42 -0600

    Adding location data for the table view

--- LetsEat/Location/LocationViewController.swift
+++ LetsEat/Location/LocationViewController.swift
@@ -11,18 +11,19 @@ import UIKit
 class LocationViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    let locations = ["Aspen","Boston","Charleston","Chicago","Houston","Las Vegas","Los Angeles","Miami","New Orleans","New York","Philadelphia","Portland","San Antonio","San Francisco","Washington District of Columbia"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return locations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = "A Cell"
+        cell.textLabel?.text = locations[indexPath.row]
         return cell
     }
 }
