dc48a4f63ad7cf136ddb4f0addc46706348ef0d2
AmauryGz <gzamaury@hotmail.com>
2021-02-25 18:31:26 -0600

    Adding annotations to a map view - Creating MapViewController

---
+++ LetsEat/Map/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  LetsEat
+//
+//  Created by MacGza on 25/02/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
