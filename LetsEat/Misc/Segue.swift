48a6ad2dfa0fd16a9092c7d5a5925128675634c1
AmauryGz <gzamaury@hotmail.com>
2021-02-26 17:19:24 -0600

    Performing the showDetail segue

---
+++ LetsEat/Misc/Segue.swift
@@ -0,0 +1,20 @@
+//
+//  Segue.swift
+//  LetsEat
+//
+//  Created by MacGza on 26/02/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import Foundation
+
+enum Segue: String {
+    case showDetail
+    case showRating
+    case showReview
+    case showAllReviews
+    case restaurantList
+    case locationList
+    case showPhotoReview
+    case showPhotoFilter
+}
