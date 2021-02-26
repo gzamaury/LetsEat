6038b1a0da2068b8dda73f1d3f057337fe49064e
AmauryGz <gzamaury@hotmail.com>
2021-02-26 16:03:07 -0600

    Creating custom annotations

--- LetsEat/Map/MapViewController.swift
+++ LetsEat/Map/MapViewController.swift
@@ -19,6 +19,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     }
     
     func initialize() {
+        mapView.delegate = self
         manager.fetch { (annotations) in addMap(annotations)
         }
     }
@@ -26,5 +27,24 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     func addMap(_ annotations: [RestaurantItem]) {
         mapView.setRegion(manager.currentRegion(latDelta: 0.5, longDelta: 0.5), animated: true)
         mapView.addAnnotations(manager.annotations)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "custompin"
+        guard !annotation.isKind(of: MKUserLocation.self) else { return nil }
+        var annotationView: MKAnnotationView?
+        if let customAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) {
+            annotationView = customAnnotationView
+            annotationView?.annotation = annotation
+        } else {
+            let av = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = av
+        }
+        if let annotationView = annotationView {
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "custom-annotation")
+        }
+        return annotationView
     }
 }
