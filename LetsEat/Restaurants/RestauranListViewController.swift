2cbff8c2536054263a260423dd00cfe21c45402d
AmauryGz <gzamaury@hotmail.com>
2021-02-26 23:44:17 -0600

    Refactoring RestaurantListViewController

--- LetsEat/Restaurants/RestauranListViewController.swift
+++ LetsEat/Restaurants/RestauranListViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class RestauranListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class RestauranListViewController: UIViewController, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+}
+
+
+// MARK: Private Extension
+private extension RestauranListViewController {
     
+}
+
+// MARK: UICollectionViewDataSource
+extension RestauranListViewController {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         1
     }
@@ -25,15 +32,4 @@ class RestauranListViewController: UIViewController, UICollectionViewDataSource,
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
