308332ed731ef75cc2e948c45fd5b9f8b2fe317f
AmauryGz <gzamaury@hotmail.com>
2021-02-27 20:58:32 -0600

    Understanding the JSON format - Creating RestaurantAPIManager

---
+++ LetsEat/Misc/JSON/RestaurantAPIManager.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantAPIManager.swift
+//  LetsEat
+//
+//  Created by MacGza on 27/02/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import Foundation
+
+struct RestaurantAPIManager {
+    static func loadJSON(file name: String) -> [[String: AnyObject]]{
+        var items = [[String: AnyObject]]()
+        guard
+            let path = Bundle.main.path(forResource: name, ofType: "json"),
+            let data = NSData(contentsOfFile: path)
+            else {
+                return [[:]]
+        }
+        do {
+            let json = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments) as AnyObject
+            if let restaurants = json as? [[String: AnyObject]] {
+                items = restaurants as [[String: AnyObject]]
+            }
+        } catch {
+            print("error serializing JSON: \(error)")
+            items = [[:]]
+        }
+        return items
+    }
+}
