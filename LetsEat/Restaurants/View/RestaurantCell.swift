dd122fb8e4ee9aa3459a6698358ed774e3112aaf
AmauryGz <gzamaury@hotmail.com>
2021-03-02 17:08:04 -0600

    Creating a view controller for the cells on the Restaurant List screen - Connecting the outlets for RestaurantCell

---
+++ LetsEat/Restaurants/View/RestaurantCell.swift
@@ -0,0 +1,16 @@
+//
+//  RestaurantCell.swift
+//  LetsEat
+//
+//  Created by MacGza on 02/03/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblCuisine: UILabel!
+    @IBOutlet weak var imgRestaurant: UIImageView!
+}
