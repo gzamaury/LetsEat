170a2734af5f7eba3deacde79c831f5906aae940
AmauryGz <gzamaury@hotmail.com>
2021-03-01 18:06:37 -0600

    Adding an unwind action method to the Done button

--- LetsEat/Explore/ExploreViewController.swift
+++ LetsEat/Explore/ExploreViewController.swift
@@ -31,14 +31,23 @@ private extension ExploreViewController {
     
     @IBAction func unwindLocationCancel(segue:UIStoryboardSegue){
     }
+    
+    @IBAction func unwindLocationDone(segue: UIStoryboardSegue) {
+        if let viewController = segue.source as? LocationViewController {
+            selectedCity = viewController.selectedCity
+            if let location = selectedCity {
+                headerView.lblLocation.text = location.full
+            }
+        }
+    }
 }
 
 // MARK: UICollectionViewDataSource
 extension ExploreViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind Kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: Kind, withReuseIdentifier: "header", for: indexPath)
-        
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: Kind, withReuseIdentifier: "header", for: indexPath)
+        headerView = header as? ExploreHeaderView
         return headerView
     }
     
