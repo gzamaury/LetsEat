b7c66c3c7fdfbd4f7a1b51ee20758f486b6454a1
AmauryGz <gzamaury@hotmail.com>
2021-03-02 19:00:24 -0600

    Creating RestaurantDataManager

---
+++ LetsEat/Restaurants/Model/RestaurantDataManager.swift
@@ -0,0 +1,41 @@
+//
+//  RestaurantDataManager.swift
+//  LetsEat
+//
+//  Created by MacGza on 02/03/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import Foundation
+
+class REstaurantDataManager {
+    private var items: [RestaurantItem] = []
+    
+    func fetch(by locations: String, with filter: String = "All", completionHandler: (_ items: [RestaurantItem]) -> Void) {
+        if let file = Bundle.main.url(forResource: locations, withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: file)
+                let restaurants = try
+                    JSONDecoder().decode([RestaurantItem].self, from: data)
+                if filter != "All" {
+                    items = restaurants.filter(
+                        { ($0.cuisines.contains(filter)) }
+                    )
+                } else {
+                    items = restaurants
+                }
+            } catch {
+                print("there was an error \(error)")
+            }
+        }
+        completionHandler(items)
+    }
+    
+    func  numberOfItems() -> Int {
+        return items.count
+    }
+    
+    func restarantItem(at index: IndexPath) -> RestaurantItem {
+        return items[index.item]
+    }
+}
