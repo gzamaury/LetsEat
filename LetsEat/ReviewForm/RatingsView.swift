3428178cbcd4460cdd33e8fa548feaab3bfc958e
AmauryGz <gzamaury@hotmail.com>
2021-03-04 17:25:27 -0600

    Creating a custom UIControl object

---
+++ LetsEat/ReviewForm/RatingsView.swift
@@ -0,0 +1,13 @@
+//
+//  RatingsView.swift
+//  LetsEat
+//
+//  Created by MacGza on 04/03/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import UIKit
+
+class RatingsView: UIControl {
+
+}
