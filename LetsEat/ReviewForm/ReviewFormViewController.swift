e12417b3aec5006f8cd14ddf30471fcf07c4c31d
AmauryGz <gzamaury@hotmail.com>
2021-03-09 18:30:27 -0600

    Updating ReviewFormViewController to save reviews

--- LetsEat/ReviewForm/ReviewFormViewController.swift
+++ LetsEat/ReviewForm/ReviewFormViewController.swift
@@ -9,20 +9,32 @@
 import UIKit
 
 class ReviewFormViewController: UITableViewController {
+    var selectedRestaurantID: Int?
+    
     @IBOutlet weak var ratingView: RatingsView!
     @IBOutlet weak var tfTitle: UITextField!
     @IBOutlet weak var tfName: UITextField!
     @IBOutlet weak var tvReview: UITextView!
-    
-    @IBAction func onSaveTapped(_ sender: Any) {
-        print(ratingView.rating)
-        print(tfTitle.text as Any)
-        print(tfName.text as Any)
-        print(tvReview.text as Any)
-        dismiss(animated: true, completion: nil)
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+
+// MARK: Private Extension
+private extension ReviewFormViewController {
+    @IBAction func onSaveTapped(_ sender: Any) {
+        var item = ReviewItem()
+        item.name = tfName.text
+        item.title = tfTitle.text
+        item.customerReview = tvReview.text
+        item.restaurantID = selectedRestaurantID
+        item.rating = Float(ratingView.rating)
+        
+        let manager = CoreDataManager()
+        manager.addReview(item)
+        
+        dismiss(animated: true, completion: nil)
     }
 }
