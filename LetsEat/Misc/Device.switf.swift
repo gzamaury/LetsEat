4173bc0f2b2a5865d54d49f71334b78ae58f378b
AmauryGz <gzamaury@hotmail.com>
2021-03-16 20:25:23 -0600

    Updating the app to work on iPad - Checking device type

---
+++ LetsEat/Misc/Device.switf.swift
@@ -0,0 +1,26 @@
+//
+//  Device.switf.swift
+//  LetsEat
+//
+//  Created by MacGza on 16/03/21.
+//  Copyright © 2021 AmauryGz. All rights reserved.
+//
+
+import UIKit
+
+struct Device {
+    static var currentDevice: UIDevice {
+        struct Singleton {
+            static let device = UIDevice.current
+        }
+        return Singleton.device
+    }
+    
+    static var isPhone: Bool {
+        return currentDevice.userInterfaceIdiom == .phone
+    }
+    
+    static var isPad: Bool {
+        return currentDevice.userInterfaceIdiom == .pad
+    }
+}
